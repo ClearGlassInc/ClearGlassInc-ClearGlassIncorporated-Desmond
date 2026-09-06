97c5c58149174a7279bab44fe8816bc96ea519a5
Desmond Otieno <77194670+ClearGlasslabs@users.noreply.github.com>
2026-09-06 08:10:19 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
-# Copyright (c) 2024-2026 ClearGlass Inc. All Rights Reserved.
-# Proprietary and confidential. See LICENSE for terms.
-"""Placeholder module."""
+"""ClearGlass engineering and marketing agent-army control plane."""
+
+from .orchestrator import AgentArmy, ExecutionPlan, load_config
+
+__all__ = ["AgentArmy", "ExecutionPlan", "load_config"]
